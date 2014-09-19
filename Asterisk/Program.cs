c4a66112ca490a5bc2651e7a4b32ee68669ffb38
kn222gp <kn222gp@student.lnu.se>
2014-09-19 13:34:00 +0200

    Remake of repo, since it was corrupt or something.

---
+++ Asterisk/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asterisk
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int rows;
+            int columns;
+
+            for (rows = 0; rows < 25; rows++)
+            {
+
+                switch (rows % 3)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                        
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                
+                }
+                if (rows % 2 == 1)
+                {
+                    Console.Write(" ");
+                }
+                for (columns = 0; columns < 39; columns++)
+                {
+                    Console.Write("* ");
+                }
+
+               
+                Console.WriteLine();
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
