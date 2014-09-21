e0c2d9b4179d427fe762bc55e4ba78a20c1ce77d
kn222gp <kn222gp@student.lnu.se>
2014-09-21 16:34:30 +0200

    Added comments

--- Asterisk/Program.cs
+++ Asterisk/Program.cs
@@ -11,12 +11,17 @@ namespace Asterisk
 
         static void Main(string[] args)
         {
+
+            // 1. Deklarera variabler.
             int rows;
             int columns;
 
+
+            // 2. Skapar for-satser. En som innehåller antal rader och en som innehåller antalet kolumner.
             for (rows = 0; rows < 25; rows++)
             {
 
+            // 3. Switch-sasten kollar vilket case det blir och sätter färg därefter. Var tredje rad gul, var tredje magenta osv...
                 switch (rows % 3)
                 {
                     case 0:
@@ -31,11 +36,13 @@ namespace Asterisk
                         Console.ForegroundColor = ConsoleColor.Green;
                         break;
                 
+                // 4. If-sasten gör så att varannan rad blir inskjuten. If sasten kan antingen bli 0 eller 1.
                 }
                 if (rows % 2 == 1)
                 {
                     Console.Write(" ");
                 }
+                // ...den andra for satsen för kolumner. Här ritas asteriskerna ut.
                 for (columns = 0; columns < 39; columns++)
                 {
                     Console.Write("* ");
